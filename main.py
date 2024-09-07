534e66789403b090dd864cc6944563a6fddf070c
William de Souza <williamssouza89@gmail.com>
2024-09-07 04:33:58 +0000

    atualizacao para tipagem retornar com o nome da coluna

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ client_glue = Glue()
 
 
 def prepare_diagram_pattern(dict_table):
-    
+    '''
+        Funcao para criar no padrao do https://dbdiagram.io
+    '''
     for table in dict_table:
         output = f'''Table {table}  {{ \n'''
 
         for column in dict_table[table]['Columns']:
-            output += f'''    {column} string \n''' 
+            
+            output += f'''    {column['Name']} {column['Type']} \n''' 
         
         output += '''}\n''' 
 
         print(output)  
-    
     pass
-
-
 
 
 
 if __name__ == '__main__':
 
-    # list_tables = client_glue.get_list_tables("database_test")
+    #list_tables = client_glue.get_list_tables("database_test")
 
     # er_diagram = {}
 
@@ -36,13 +36,19 @@ if __name__ == '__main__':
     #     columns = table['Columns']
 
     #     er_diagram[table_name] = {'Columns': []}
+
     #     for column in columns:
-    #         er_diagram[table_name]['Columns'].append(column['Name'])
+    #         column_info = {
+    #             'Name': column['Name'],
+    #             'Type': column['Type']
+    #         }
 
-    # print(er_diagram)
+    #         er_diagram[table_name]['Columns'].append(column_info)
 
-    dict_tables = {'table1_test': {'Columns': ['id', 'descritivo1', 'descritivo2', 'descritivo3']}, 'table_test_2': {'Columns': []}}
-
-    prepare_diagram_pattern(dict_tables)
+    #print(er_diagram)
     
+        
+    er_diagram = {'table1_test': {'Columns': [{'Name': 'id', 'Type': 'bigint'}, {'Name': 'descritivo1', 'Type': 'string'}, {'Name': 'descritivo2', 'Type': 'string'}, {'Name': 'descritivo3', 'Type': 'string'}]}, 'table_test_2': {'Columns': [{'Name': 'id', 'Type': 'bigint'}, {'Name': 'id_table_1', 'Type': 'bigint'}, {'Name': 'descritivo1', 'Type': 'string'}, {'Name': 'descritivo2', 'Type': 'string'}, {'Name': 'descritivo3', 'Type': 'string'}, {'Name': 'descritivo4', 'Type': 'string'}, {'Name': 'descritivo5', 'Type': 'string'}]}}
 
+    prepare_diagram_pattern(er_diagram)
+    
